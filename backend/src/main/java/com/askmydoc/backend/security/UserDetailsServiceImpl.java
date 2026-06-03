d948ed93c9131603b0d994dc206fe259f73cb3c9
Harsh Rana <harshrana8454@gmail.com>
2026-06-03 16:27:49 +0530

    security+jwt added

---
+++ backend/src/main/java/com/askmydoc/backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.askmydoc.backend.security;
+
+import com.askmydoc.backend.model.User;
+import com.askmydoc.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User appUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(appUser.getEmail())
+                .password(appUser.getPassword())
+                .authorities(new SimpleGrantedAuthority("ROLE_" + appUser.getRole().name()))
+                .build();
+    }
+}
